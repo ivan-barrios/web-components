3bfe9d413c946281804e72478ec07811c7ea9b9c
Ivan Barrios <ivanbar8112003@gmail.com>
2023-05-21 21:48:04 -0300

    Navbar structure generalized

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,20 +16,24 @@ const Navbar = () => (
     {/* The following div is only to show the gradient */}
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className="2xl:max-w-[1280px] w-full mx-auto flex justify-between gap-8">
-      <Image
-        src="/search.svg"
-        alt="Search"
-        className="object-contain"
-        width={24}
-        height={24}
-      />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        WEBPAGE TITLE
+        LOGO/TITLE
       </h2>
+      <ul className="hidden md:flex gap-12 mr-8 text-xl">
+        <li className="text-white font-medium">
+          <a href="#">NavElement1</a>
+        </li>
+        <li className="text-white font-medium">
+          <a href="#">NavElement2</a>
+        </li>
+        <li className="text-white font-medium">
+          <a href="#">NavElement3</a>
+        </li>
+      </ul>
       <Image
         src="/menu.svg"
         alt="Menu"
-        className="object-contain"
+        className="object-contain md:hidden"
         width={24}
         height={24}
       />
